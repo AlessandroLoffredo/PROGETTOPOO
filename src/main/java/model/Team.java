81ea1fe8a9a87ad22c8d9c4fde7052b3a458d365
DomenicoGuerrera <dom.guerrera@studenti.unina.it>
2025-04-23 17:58:46 +0200

    modifiche finale alle classi prima della consegna

--- src/main/java/model/Team.java
+++ src/main/java/model/Team.java
@@ -3,7 +3,8 @@ package model;
 import java.util.ArrayList;
 
 public class Team {
-    private short mark;
+    private short finalMark;
+    private ArrayList<Integer> judgesMark;
     private final String nickname;
     private ArrayList<Participant> parList;
     private Hackathon hackathon;
@@ -13,18 +14,20 @@ public class Team {
     //COSTRUTTORE
     public Team(String nickname, Hackathon hackathon){
         this.nickname = nickname;
+        this.judgesMark = new ArrayList<>();
+        this.finalMark = 0;
         this.parList = new ArrayList<>();
         this.hackathon = hackathon;
         this.docList = new ArrayList<>();
     }
 
     //METODI
-    public short getMark(){
-        return mark;
+    public short getFinalMark(){
+        return finalMark;
     }
 
-    public void setMark(short mark) {
-        this.mark = mark;
+    public void addMark(short mark) {
+        this.judgesMark.add((int)mark);
     }
 
     public String getNickname(){
@@ -47,4 +50,15 @@ public class Team {
         this.docList.add(doc);
     }
 
+    public void avgMark(){
+        for (int mark : this.judgesMark) {
+            this.finalMark += (short) mark;
+        }
+        if (!this.judgesMark.isEmpty()) {
+            this.finalMark /= this.judgesMark.size();
+            this.finalMark = (short) Math.round(this.finalMark);
+        } else {
+            System.out.println("La lista dei punteggi è vuota. Impossibile calcolare il finalMark.");
+        }
+    }
 }
